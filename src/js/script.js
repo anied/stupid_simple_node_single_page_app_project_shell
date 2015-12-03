d9a97919bb0282f14a907c1c26419221fb4da4ba
Alexander Nied <awnied@gmail.com>
2015-12-02 21:37:44 -0600

    POC with working template loading.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
 console.log('script loaded!!!');
+
+$(document).ready(function () {
+	$.ajax({url: '/html/template.html'})
+		.done(function (data, textStatus, jqXHR) {
+			$('#templateTarget').append($(data));
+		});
+});
