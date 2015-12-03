4b0f5da2aeb9d6b8161e47de460c97bce2ce9b1b
Alexander Nied <awnied@gmail.com>
2015-12-03 00:42:11 -0600

    Cleared out supplementary demo material to get the repo to a "bare bones" state.  If much future development is required on this project, it would probably make sense to automate this step in some kind of build process.

--- src/js/script.js
+++
@@ -1,8 +0,0 @@
-console.log('javasscript file loaded as expected!!!');
-
-$(document).ready(function () {
-	$.ajax({url: '/html/template.html'})
-		.done(function (data, textStatus, jqXHR) {
-			$('#templateTarget').append($(data));
-		});
-});
